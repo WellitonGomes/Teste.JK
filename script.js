94bc45bbed1637b7a91ce1b1bede3ad7374b0230
Welliton da Silva Gomes <137785960+WellitonGomes@users.noreply.github.com>
2024-12-02 15:41:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+//gerando um número aleatório entre 1 e 100
+
+function verificar() { //função para verificar o numero do usuario
+    const guess = parseInt(document.getElementById("guess").value);
+    //pega o valor digitado pelo usuario e converte para inteiro 
+    const resultado = document.getElementById("resultado");
+    //pega a div com o valor digitado
+    const botaoReiniciar = document.getElementById("reiniciar");
+
+    if (isNaN(guess)) { //if para caso não seja digitado um numero 
+        resultado.textContent = "Por favor, insira um número válido.";
+        return;
+    }
+
+    //if para verificar o numero digitado 
+    if (guess === numeroAleatorio) {
+        //se o valor for o mesmo mostra a mensagem 
+        resultado.textContent = "Parabéns! Você acertou!";
+    //se o valor não estiver correto mostra estas mensagens dizendo se o numero é maior ou menor do que o que foi inserido
+        botaoReiniciar.style.display = "block";
+    } else if (guess < numeroAleatorio) {
+        resultado.textContent = "Tente novamente! O número é maior.";
+    } else {
+        resultado.textContent = "Tente novamente! O número é menor.";
+    }
+}
+
+//função para reiniciar o jogo 
+function reiniciarJogo() {
+     location.reload(); // Recarrega a página para reiniciar o jogo 
+}
